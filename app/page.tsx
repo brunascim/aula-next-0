6751ed0a30ea3f61d5487efb7229bd21a755e026
brunascim <127804993+brunascim@users.noreply.github.com>
2024-03-11 11:44:47 -0300

    aula 2 -11/03

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,13 @@ export default function Page() {
       <h1>Hello, Next.js!</h1>
       <h2>Título 2</h2>
       <h3>Título 3</h3>
-      Qualquer coisa
+      
+      <ul>
+        <li><Link href={'/sobre'}>Sobre</Link></li>
+        <li><Link href={'/cadusuario'}>Cadastro de Usuário</Link></li>
+      </ul>
 
-    <Link href={'/sobre'}>Sobre</Link>
-    <Link href={'/cadusuario'}>Cadastro de Usuário</Link>
+    
     </>
   );
 }
--- app/usuarios/cadusuario/page.tsx
+++ app/usuarios/cadusuario/page.tsx
@@ -1,8 +1,13 @@
+import Link from 'next/link';
+
 export default function CadUsuario () {
     return(
         <>
         <h1>Cadastro</h1>
         <p>Insira as informações sobre o usuário</p>
+
+        <p><Link href={'/'}>Voltar</Link></p>
         </>
     );
 }
+
